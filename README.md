327ff2fef16dca3bc4cbe87c8148e2cf94a81dc2
elsirion <elsirion@protonmail.com>
2025-11-21 22:03:13 +0100

    chore: add docs

---
+++ README.md
@@ -0,0 +1,60 @@
+# Blitzi: Lightning made easy
+
+Easy to use Bitcoin Lighning client that uses [Fedimint](https://fedimint.org/) as its backend.
+
+You want to build a lightning powered app but don't look forward to dealing
+with the complexity of running your own Lightning node? Blitzi is for you!
+With Blitzi you can outsource the infrastructure to any Fedimint federation
+of your choosing (or just go with the default for small amounts) and receive
+and send Lightning payments without any hassle.
+
+## Examples
+
+The fastest way to get started is to create a new Blitzi client with default
+settings. This is only advisable for small amounts since it will use a
+default Fedimint federation which the author of this library trusts, but
+ultimately can't guarantee the security of. For larger amounts we recommend
+making your own choice which federation to use based on your own due
+diligence.
+
+```rust
+# use anyhow::Result;
+use blitzi::Blitzi;
+
+# #[tokio::main]
+# async fn main() -> Result<()> {
+// Create a new Blitzi client with default settings
+let blitzi = Blitzi::new().await?;
+
+// Generate a new Lightning invoice for 1000 millisatoshi and await its payment
+let invoice = blitzi.lightning_invoice(1000, "Test payment").await?;
+println!("Invoice: {}", invoice);
+
+match blitzi.await_incoming_payment(&invoice).await {
+    Ok(()) => println!("Payment received"),
+    Err(_) => println!("Invoice expired"),
+}
+
+# Ok(())
+# }
+```
+
+## Fedimint
+
+Blitzi uses Fedimint, an open source federated ecash mint implementation on
+Bitcoin, to connect you to the Lighning network. Federated in this context
+means that each federation is run by a group of people, also called
+guardians, who are jointly responsible for the security of the funds held in
+the federation. This means, while no signle guardian can steal your funds,
+if a majority of the guardians are compromised, the funds are at risk, so
+chose your federation wisely.
+
+The default federation used by Blitzi is [E-Cash Club], which for various
+reasons seems the most reasonable choice at the time of writing (long run
+time, multiple ASNs, etc.). For anything but toy amounts users should make
+their own choice though. You can find a list of publicly known federations
+on [Fedimint Observer], which also provices statistics and uptime statistics
+about them.
+
+[E-Cash Club]: (https://observer.fedimint.org/federations/aeca6cc80ffc530bd2d54b09681f6edb9a415c362e4af2fe3d5e04137006fa21)
+[Fedimint Observer]: (https://observer.fedimint.org/)
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,69 @@
+//! Easy to use Bitcoin Lighning client that uses [Fedimint](https://fedimint.org/) as its backend.
+//!
+//! You want to build a lightning powered app but don't look forward to dealing
+//! with the complexity of running your own Lightning node? Blitzi is for you!
+//! With Blitzi you can outsource the infrastructure to any Fedimint federation
+//! of your choosing (or just go with the default for small amounts) and receive
+//! and send Lightning payments without any hassle.
+//!
+//! # Examples
+//! The fastest way to get started is to create a new Blitzi client with default
+//! settings. This is only advisable for small amounts since it will use a
+//! default Fedimint federation which the author of this library trusts, but
+//! ultimately can't guarantee the security of. For larger amounts we recommend
+//! making your own choice which federation to use based on your own due
+//! diligence.
+//!
+//! ```no_run
+//! # use anyhow::Result;
+//! use blitzi::Blitzi;
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<()> {
+//! // Create a new Blitzi client with default settings
+//! let blitzi = Blitzi::new().await?;
+//!
+//! // Generate a new Lightning invoice for 1000 millisatoshi and await its payment
+//! let invoice = blitzi.lightning_invoice(1000, "Test payment").await?;
+//! println!("Invoice: {}", invoice);
+//!
+//! match blitzi.await_incoming_payment(&invoice).await {
+//!     Ok(()) => println!("Payment received"),
+//!     Err(_) => println!("Invoice expired"),
+//! }
+//!
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Fedimint
+//! Blitzi uses Fedimint, an open source federated ecash mint implementation on
+//! Bitcoin, to connect you to the Lighning network. Federated in this context
+//! means that each federation is run by a group of people, also called
+//! guardians, who are jointly responsible for the security of the funds held in
+//! the federation. This means, while no signle guardian can steal your funds,
+//! if a majority of the guardians are compromised, the funds are at risk, so
+//! chose your federation wisely.
+//!
+//! The default federation used by Blitzi is [E-Cash Club], which for various
+//! reasons seems the most reasonable choice at the time of writing (long run
+//! time, multiple ASNs, etc.). For anything but toy amounts users should make
+//! their own choice though. You can find a list of publicly known federations
+//! on [Fedimint Observer], which also provices statistics and uptime statistics
+//! about them.
+//!
+//! [E-Cash Club]: (https://observer.fedimint.org/federations/aeca6cc80ffc530bd2d54b09681f6edb9a415c362e4af2fe3d5e04137006fa21)
+//! [Fedimint Observer]: (https://observer.fedimint.org/)
+
 use std::path::PathBuf;
 use std::str::FromStr;
 
+use anyhow::{Context, anyhow, ensure};
 use fedimint_bip39::{Bip39RootSecretStrategy, Mnemonic};
 use fedimint_client::meta::MetaService;
 use fedimint_client::module::meta::LegacyMetaSource;
 use fedimint_client::secret::RootSecretStrategy;
 use fedimint_client::{Client, ClientHandle, ClientModuleInstance, RootSecret};
-use fedimint_core::anyhow::{Context, anyhow, ensure};
 use fedimint_core::bitcoin::hashes::sha256;
 use fedimint_core::core::OperationId;
 use fedimint_core::db::{Database, IRawDatabaseExt};
@@ -23,6 +80,23 @@ use lightning_invoice::{Bolt11Invoice, Bolt11InvoiceDescription, Description};
 
 const ECASH_CLUB_INVITE: &str = "fed11qgqzggnhwden5te0v9cxjtn9vd3jue3wvfkxjmnyva6kzunyd9skutnwv46z7qqpyzhv5mxgpl79xz7j649sj6qldmde5s2uxchy4uh7840qgymsqmazzp6sn43";
 
+/// Builder for the Blitzi client that allows configuring the fedimint client's
+/// settings.
+///
+/// ```no_run
+/// # use anyhow::Result;
+/// use blitzi::Blitzi;
+///
+/// # #[tokio::main]
+/// # async fn main() -> Result<()> {
+/// let blitzi = Blitzi::builder()
+///     .datadir("/path/to/data")
+///     .federation("fed11qgqzggnhwden5te0v9cxjtn9vd3jue3wvfkxjmnyva6kzunyd9skutnwv46z7qqpyzhv5mxgpl79xz7j649sj6qldmde5s2uxchy4uh743")?
+///     .build()
+///     .await?;
+/// # Ok(())
+/// # }
+/// ```
 pub struct BlitziBuilder {
     datadir: PathBuf,
     federation: InviteCode,
@@ -45,8 +119,8 @@ impl Default for BlitziBuilder {
 impl BlitziBuilder {
     /// Sets the directory where Fedimint data will be stored. Defaults to
     /// `$XDG_DATA_HOME/fedimint/default`
-    pub fn datadir(mut self, path: PathBuf) -> Self {
-        self.datadir = path;
+    pub fn datadir(mut self, path: impl Into<PathBuf>) -> Self {
+        self.datadir = path.into();
         self
     }
 
@@ -66,6 +140,14 @@ impl BlitziBuilder {
         Ok(self)
     }
 
+    /// Builds the Blitzi client.
+    ///
+    /// This function will open the existing Fedimint client or join the
+    /// federation depending on whether the client has already been initialized.
+    ///
+    /// # Errors
+    /// Returns an error if the database cannot be opened or if joining the
+    /// federation fails.
     pub async fn build(self) -> anyhow::Result<Blitzi> {
         let mut client_builder = fedimint_client::Client::builder().await?;
         client_builder.with_module(MintClientInit);
@@ -120,6 +202,27 @@ async fn generate_root_secret(db: &Database) -> anyhow::Result<RootSecret> {
     )))
 }
 
+/// The Blitzi client that allows paying and receiving payments on Lightning.
+///
+/// ```no_run
+/// # use anyhow::Result;
+/// # use fedimint_core::hex;
+/// use blitzi::Blitzi;
+///
+/// # #[tokio::main]
+/// # async fn main() -> Result<()> {
+/// let blitzi = Blitzi::new().await?;
+///
+/// // Amount is in millisatoshi
+/// let invoice = blitzi.lightning_invoice(1000, "Test payment").await?;
+/// println!("Invoice: {}", invoice);
+///
+/// let preimage = blitzi.pay(&invoice).await?;
+/// println!("Preimage: {}", hex::encode(preimage));
+///
+/// # Ok(())
+/// # }
+/// ```
 pub struct Blitzi {
     client: ClientHandle,
 }
@@ -142,6 +245,12 @@ impl Blitzi {
             .expect("LN module not found")
     }
 
+    /// Generates a new Lightning invoice for the given `amount_msats` in
+    /// millisatoshi containing the given `description`.
+    ///
+    /// # Errors
+    /// Returns an error if no LN gateway is available or if the invoice cannot
+    /// be generated for any other reason.
     pub async fn lightning_invoice(
         &self,
         amount_msats: u64,
